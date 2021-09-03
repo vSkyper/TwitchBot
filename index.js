76083c92b955f1459e0971df715cd46f2cf00d1b
Mateusz Piwowarski <skyperpl@gmail.com>
2021-09-03 22:10:34 +0200

    #1

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const tmi = require('tmi.js');
 require('dotenv').config();
 
-
 const client = new tmi.Client({
-	options: { debug: true, messagesLogLevel: "info" },
-	connection: {
-		reconnect: true,
-		secure: true
-	},
-	identity: {
-		username: 'zadymka_bot',
-		password: process.env.TWITCH_OAUTH_TOKEN
-	},
-	channels: [ 'vSkyper' ]
+  options: { debug: true, messagesLogLevel: 'info' },
+  connection: {
+    reconnect: true,
+    secure: true,
+  },
+  identity: {
+    username: 'zadymka_user',
+    password: process.env.TWITCH_OAUTH_TOKEN,
+  },
+  channels: ['vSkyper'],
 });
+
 client.connect().catch(console.error);
+
 client.on('message', (channel, tags, message, self) => {
-	if(self) return;
-	if(message.toLowerCase() === '!hello') {
-		client.say(channel, `@${tags.username}, heya!`);
-	}
+  if (self) return;
+
+  switch (message) {
+    case 'PogU':
+      client.say(channel, 'PogU');
+      break;
+    case 'EZ':
+      client.say(channel, 'EZ');
+      break;
+    case ':tf:':
+      client.say(channel, ':tf:');
+      break;
+  }
+
+  if (message.includes('XD')) {
+    client.say(channel, 'XDDDDDDDD');
+  }
+
+  switch (tags.username) {
+    case '17norbert':
+      client.say(channel, `@${tags.username} antyzadymkowicz Porvalo`);
+      break;
+  }
 });
