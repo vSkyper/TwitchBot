105f349d611c3a8aed6adf8cb9013be09ea75b7b
Mateusz Piwowarski <skyperpl@gmail.com>
2021-09-03 20:37:32 +0200

    init

--- index.js
+++ index.js
@@ -1,64 +1,23 @@
-const { Client, MessageAttachment, MessageEmbed } = require('discord.js');
-const axios = require('axios');
+const tmi = require('tmi.js');
 require('dotenv').config();
 
-const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
-const prefix = '>';
 
-const randomImage = (message, url) => {
-  axios
-    .get(url)
-    .then((res) => {
-      const embed = new MessageEmbed()
-        .setColor('#f1c40f')
-        .setTitle('✨')
-        .setImage(res.data.link);
-
-      message.channel.send({ embeds: [embed] });
-    })
-    .catch((error) => console.log(error));
-};
-
-const rainbow = (message) => {
-  const embed = new MessageEmbed()
-    .setColor('#f1c40f')
-    .setTitle('🌈')
-    .setImage(
-      `https://some-random-api.ml/canvas/gay/?avatar=${message.author.avatarURL(
-        {
-          format: 'png',
-        }
-      )}`
-    );
-  message.channel.send({ embeds: [embed] });
-};
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const client = new tmi.Client({
+	options: { debug: true, messagesLogLevel: "info" },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: 'zadymka_bot',
+		password: process.env.TWITCH_OAUTH_TOKEN
+	},
+	channels: [ 'vSkyper' ]
 });
-
-client.on('messageCreate', (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-  const args = message.content.trim().slice(prefix.length).split(/ +/);
-  const command = args.shift().toLowerCase();
-
-  switch (command) {
-    case 'hello':
-      message.reply(`Hi ${message.author.username}!`);
-      break;
-    case 'cat':
-      randomImage(message, 'https://some-random-api.ml/img/cat');
-      break;
-    case 'panda':
-      randomImage(message, 'https://some-random-api.ml/img/panda');
-      break;
-    case 'rainbow':
-      rainbow(message);
-      break;
-    default:
-      message.reply('Invalid command used.');
-  }
+client.connect().catch(console.error);
+client.on('message', (channel, tags, message, self) => {
+	if(self) return;
+	if(message.toLowerCase() === '!hello') {
+		client.say(channel, `@${tags.username}, heya!`);
+	}
 });
-
-client.login(process.env.DISCORD_BOT_TOKEN);
