6ef45915ee7957fabc46c3db5057dead68bb0e0c
Mateusz Piwowarski <skyperpl@gmail.com>
2021-09-04 11:28:55 +0200

    #2

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const tmi = require('tmi.js');
 require('dotenv').config();
 
+let active = true;
+const cooldown = () => {
+  active = false;
+  console.log('Cooldown starts');
+  setTimeout(() => {
+    active = true;
+    console.log('Cooldown ends');
+  }, 10000);
+};
+
 const client = new tmi.Client({
   options: { debug: true, messagesLogLevel: 'info' },
   connection: {
@@ -17,27 +27,29 @@ const client = new tmi.Client({
 client.connect().catch(console.error);
 
 client.on('message', (channel, tags, message, self) => {
-  if (self) return;
+  if (self || !active) return;
 
   switch (message) {
     case 'PogU':
       client.say(channel, 'PogU');
+      cooldown();
+      break;
+    case 'OMEGALUL':
+      client.say(channel, 'OMEGALUL');
+      cooldown();
       break;
     case 'EZ':
       client.say(channel, 'EZ');
+      cooldown();
       break;
     case ':tf:':
       client.say(channel, ':tf:');
+      cooldown();
       break;
   }
 
-  if (message.includes('XD')) {
+  if (message.toLowerCase().includes('xd')) {
     client.say(channel, 'XDDDDDDDD');
-  }
-
-  switch (tags.username) {
-    case '17norbert':
-      client.say(channel, `@${tags.username} antyzadymkowicz Porvalo`);
-      break;
+    cooldown();
   }
 });
